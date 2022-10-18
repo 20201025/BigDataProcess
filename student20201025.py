d311ccab4554a8d0e663a378e4ea482b860b161b
20201025 <20201025@dongduk.ac.kr>
2022-10-18 09:57:35 -0700

    hw2

--- student20201025.py
+++ student20201025.py
@@ -4,6 +4,7 @@ import openpyxl
 
 wb = openpyxl.load_workbook("student.xlsx")
 ws = wb['Sheet1']
+total = list()
 
 row_id = 1;
 for row in ws:
@@ -13,6 +14,26 @@ for row in ws:
 		sum_v += ws.cell(row = row_id, column = 5).value * 0.34
 		sum_v += ws.cell(row = row_id, column = 6).value
 		ws.cell(row = row_id, column = 7).value = sum_v
+		total.append(sum_v)
+	row_id += 1
+
+total.sort(reverse = True)
+
+row_id = 1
+for row in ws:
+	if row_id != 1:
+		if ws.cell(row = row_id, column = 7).value >= total[10]:
+			ws.cell(row = row_id, column = 8).value = "A+"
+		elif total[22] <=  ws.cell(row = row_id, column = 7).value <= total[11]:
+			ws.cell(row = row_id, column = 8).value = "A"
+		elif total[36] <= ws.cell(row = row_id, column = 7).value <= total[23]:
+			ws.cell(row = row_id, column = 8).value = "B+"
+		elif total[51] <= ws.cell(row = row_id, column = 7).value <= total[37]:
+                        ws.cell(row = row_id, column = 8).value = "B"
+		elif total[62] <= ws.cell(row = row_id, column = 7).value <= total[52]:
+                        ws.cell(row = row_id, column = 8).value = "C+"
+		elif total[73] <= ws.cell(row = row_id, column = 7).value <= total[63]:
+                        ws.cell(row = row_id, column = 8).value = "C"
 	row_id += 1
 
 wb.save("student.xlsx")
